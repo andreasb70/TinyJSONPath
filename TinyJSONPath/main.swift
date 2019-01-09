042c94d6b8138ebb0767b2a4a40b285c3e6563e0
Andi <andreasb@vmlabs.de>
2019-01-09 18:17:50 +0100

    Initial Commit

---
+++ TinyJSONPath/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TinyJSONPath
+//
+//  Created by Andi on 09.01.19.
+//  Copyright © 2019 Andreas Binner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
